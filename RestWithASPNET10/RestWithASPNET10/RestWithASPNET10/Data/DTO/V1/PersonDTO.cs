272630a4fa70555ef1567717bac9ece9acac0391
Max <mwilliax1@gmail.com>
2026-02-10 00:01:26 -0300

    Utilizando o Serializer no projeto Classe responsável pela serialização e formatação do campo data e hora.

--- RestWithASPNET10/RestWithASPNET10/RestWithASPNET10/Data/DTO/V1/PersonDTO.cs
+++ RestWithASPNET10/RestWithASPNET10/RestWithASPNET10/Data/DTO/V1/PersonDTO.cs
@@ -1,11 +1,26 @@
-﻿namespace RestWithASPNET10.Data.DTO.V1
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNET10.Data.DTO.V1
 {
     public class PersonDTO
 	{
+		[JsonPropertyOrder(2)]
+		[JsonPropertyName("code")]
 		public long Id { get; set; }
+
+		[JsonPropertyOrder(3)]
+		[JsonPropertyName("first_name")]
         public string FirstName { get; set; }
+
+		[JsonPropertyOrder(4)]
+		[JsonPropertyName("last_name")]
 		public string LastName { get; set; }
+
+		[JsonPropertyOrder(1)]
 		public string Address { get; set; }
+
+		[JsonPropertyOrder(5)]
 		public string Gender { get; set; }
+		
     }
 }
