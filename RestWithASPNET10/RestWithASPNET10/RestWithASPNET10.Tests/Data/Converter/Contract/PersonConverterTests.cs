040e99266911f3ba7888a280049a30c588766ecd
Max <mwilliax1@gmail.com>
2026-02-09 23:21:18 -0300

    Criando testes unitários para converterList

--- RestWithASPNET10/RestWithASPNET10/RestWithASPNET10.Tests/Data/Converter/Contract/PersonConverterTests.cs
+++ RestWithASPNET10/RestWithASPNET10/RestWithASPNET10.Tests/Data/Converter/Contract/PersonConverterTests.cs
@@ -71,7 +71,7 @@ namespace RestWithASPNET10.Tests
 			//Assert
 			person
 				.Should()
-                    .BeNull();
+                .BeNull();
 		}
 
 		[Fact]
@@ -126,7 +126,171 @@ namespace RestWithASPNET10.Tests
 			//Assert
 			person
 				.Should()
-					.BeNull();
+				.BeNull();
+		}
+
+		[Fact]
+		public void ParseListShouldConverterPersonDTOListToPersonList()
+		{
+            //Arrange
+            var dtoList = new List<PersonDTO>()
+            {
+                new PersonDTO
+                {
+                    Id = 1,
+                    FirstName = "Mahtma",
+                    LastName = "Gandhi",
+                    Address = "Porbandan - India",
+                    Gender = "Male"
+				},
+                new PersonDTO
+				{
+					Id = 2,
+					FirstName = "Indira",
+					LastName = "Gandhi",
+					Address = "Allahabad - India",
+					Gender = "Female"
+				}
+            };
+
+            //Act
+            var personList = _converter.ParseList(dtoList);
+
+            //Assert
+            personList
+				.Should()
+				.NotBeNull();
+            personList
+				.Should()
+				.HaveCount(2);
+
+            personList[0].Should().BeEquivalentTo(new Person
+            {
+				Id = 1,
+				FirstName = "Mahtma",
+				LastName = "Gandhi",
+				Address = "Porbandan - India",
+				Gender = "Male"
+			});
+
+			personList[1].Should().BeEquivalentTo(new Person
+			{
+				Id = 2,
+				FirstName = "Indira",
+				LastName = "Gandhi",
+				Address = "Allahabad - India",
+				Gender = "Female"
+			});
+
+            personList[0].FirstName
+                .Should()
+                .Be("Mahtma");
+            personList[1].FirstName
+                .Should()
+                .Be("Indira");
+            personList[0].LastName
+                .Should()
+                .Be("Gandhi");
+            personList[1].LastName
+                .Should()
+                .Be("Gandhi");
+		}
+
+        [Fact]
+        public void ParseNullListPersonDTOToPersonShouldReturnNull()
+        {
+            //Arrange
+            List<PersonDTO> dtoList = null;
+            //Act
+            var personList = _converter.ParseList(dtoList);
+            //Assert
+            personList
+                .Should()
+                .BeNull();
+		}
+
+		[Fact]
+		public void ParseListShouldConverterPersonListToPersonDTOList()
+		{
+			//Arrange
+			var personList = new List<Person>()
+			{
+				new Person
+				{
+					Id = 1,
+					FirstName = "Mahtma",
+					LastName = "Gandhi",
+					Address = "Porbandan - India",
+					Gender = "Male"
+				},
+				new Person
+				{
+					Id = 2,
+					FirstName = "Indira",
+					LastName = "Gandhi",
+					Address = "Allahabad - India",
+					Gender = "Female"
+				}
+			};
+
+			//Act
+			var dtoList = _converter.ParseList(personList);
+
+			//Assert
+			personList
+				.Should()
+				.NotBeNull();
+			personList
+				.Should()
+				.HaveCount(2);
+
+			personList[0]
+				.Should()
+				.BeEquivalentTo(new PersonDTO
+			{
+				Id = 1,
+				FirstName = "Mahtma",
+				LastName = "Gandhi",
+				Address = "Porbandan - India",
+				Gender = "Male"
+			});
+
+			personList[1]
+				.Should()
+				.BeEquivalentTo(new PersonDTO
+			{
+				Id = 2,
+				FirstName = "Indira",
+				LastName = "Gandhi",
+				Address = "Allahabad - India",
+				Gender = "Female"
+			});
+
+			personList[0].FirstName
+				.Should()
+				.Be("Mahtma");
+			personList[1].FirstName
+				.Should()
+				.Be("Indira");
+			personList[0].LastName
+				.Should()
+				.Be("Gandhi");
+			personList[1].LastName
+				.Should()
+				.Be("Gandhi");
+		}
+
+		[Fact]
+		public void ParseNullListPersonToPersonDTOShouldReturnNull()
+		{
+			//Arrange
+			List<Person> personList = null;
+			//Act
+			var dtoList = _converter.ParseList(personList);
+			//Assert
+			personList
+				.Should()
+				.BeNull();
 		}
 	}
 }
